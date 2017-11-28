6844269791ce581b41308dfc9d78d195925b8bee
Qian Linfeng <thewawar@gmail.com>
2017-11-29 01:58:21 +0800

    Nearly work with react ssr (official example)

---
+++ server/render.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import {renderToString} from 'react-dom/server';
+
+import App from '../src/components/App';
+
+var ssr = require('../native');
+
+let assets;
+if (process.env.NODE_ENV === 'development') {
+  // Use the bundle from create-react-app's server in development mode.
+  assets = {
+    'main.js': '/static/js/bundle.js',
+    'main.css': '',
+  };
+} else {
+  assets = require('../build/asset-manifest.json');
+}
+
+export default function render() {
+  var html = ssr.renderToString(<App assets={assets} />);
+  // There's no way to render a doctype in React so prepend manually.
+  // Also append a bootstrap script tag.
+  return '<!DOCTYPE html>' + html;
+}
